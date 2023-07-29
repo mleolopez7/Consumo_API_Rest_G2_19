de938a321933a616ea83fd37d746b4d861ff67a4
mleolopez7 <mleolopez7@gmail.com>
2023-07-29 13:13:23 -0600

    Funcio update y eliminar
    Queda mucho por hacer en esas 2, pero podemos usarlo de guia segun como dijo el lic.

--- JS/Vuelo.js
+++ JS/Vuelo.js
@@ -26,7 +26,7 @@ function CargarVuelos(){
             '<td>'+ MiItems[i].cantidad_pasajeros +'</td>'+
             '<td>'+ MiItems[i].tipo_avion +'</td>'+
             '<td>'+ MiItems[i].distancia_km +'</td>'+
-            '<td>'+
+            '<td> '+
             '<button id="btneditar" class="btn btn-primary" onclick="CargarVuelo('+ MiItems[i].codigo_vuelo +')">Editar</button>'+
             '</td>'+
             '<td>'+
@@ -95,17 +95,69 @@ function CargarVuelo(p_codigo_vuelo){
                 $('#CANTIDADPAS').val(MiItems[i].cantidad_pasajeros);
                 $('#TIPOAVI').val(MiItems[i].tipo_avion);
                 $('#DISTANCIA').val(MiItems[i].distancia_km);
-                var btnactualizar = '<input type="submit" class="btn btn-primary" id="btnactualizar" onclick="ActualizarVuelo('+ MiItems[i].codigo_vuelo +')" value="Actualizar Vuelo">';
-                $('#btnagregarvuelo').html(btnactualizar)
+                var btnactualizar = '<input type="submit" class="btn btn-primary" ' +
+                'id="btnagregar" onclick="ActualizarVuelo('+ MiItems[i].codigo_vuelo +')" value="Actualizar Vuelo">';
+                $('#btnagregarvuelo').html(btnactualizar);
             }
-        },
+        }
     });
 }
 
 function ActualizarVuelo(p_codigo_vuelo){
 
+    var datosvueloactu = {
+        codigo_vuelo :$('#CODIGOVUE').val(),
+        ciudad_origen :$('#CIUDADORI').val(),
+        ciudad_destino :$('#CIUDADDES').val(),
+        fecha_vuelo :$('#FECHAVUE').val(),
+        cantidad_pasajeros :$('#CANTIDADPAS').val(),
+        tipo_avion :$('#TIPOAVI').val(),
+        distancia_km :$('#DISTANCIA').val()
+    };
+
+    var datosvueloactujson = JSON.stringify(datosvueloactu);
+    //alert(datosvuelojson)
+
+    $.ajax({
+        url : UrlApiUpdate,
+        type: 'PUT',
+        data : datosvueloactujson,
+        datatype: 'JSON',
+        contentType : 'application/json',
+        success : function(respuesta){
+            //console.log(respuesta)
+            alert('Vuelo actualizado de forma correcta');
+            $('#Miformulario').submit();
+        },
+        error : function(textError, errorThrown){
+            alert('Error: '+ textError + errorThrown);
+        }
+    });
+    alert('Aviso');
 }
 
 function EliminarVuelo(p_codigo_vuelo){
 
+    var datosvuelo = {
+        codigo_vuelo : p_codigo_vuelo
+    };
+
+    var datosvuelojson = JSON.stringify(datosvuelo);
+
+    $.ajax({
+        url : UrlApiDelete,
+        type: 'DELETE',
+        data : datosvuelojson,
+        datatype: 'JSON',
+        contentType : 'application/json',
+        success : function(respuesta){
+            //console.log(respuesta)
+            alert('Vuelo eliminado de forma correcta');
+            $('#Miformulario').submit();
+        },
+        error : function(textError, errorThrown){
+            alert('Error: '+ textError + errorThrown);
+        }
+    });
+    alert('Aviso');
 }

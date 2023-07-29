30fe8c92a5f4ecf1198723589914441cfd09ef1f
mleolopez7 <mleolopez7@gmail.com>
2023-07-28 21:39:30 -0600

    cambios generales 2

--- ConsumoAPI_G2_19/server.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-var app = require('./app'),
-	server = app.listen(app.get('port'), () => {
-		console.log(`Iniciando Express en el puerto ${app.get('port')}`)
-	})
--- JS/Vuelo.js
+++ JS/Vuelo.js
@@ -1,14 +1,14 @@
 var UrlApiGetAll = 'http://localhost:5002/vuelo/getAll';
+var UrlApiInsert = 'http://localhost:5002/vuelo/insertar/:codigo_vuelo';
 var UrlApiGetUno = 'http://localhost:5002/vuelo/getOne/:codigo_vuelo';
-var UrlApiInsert = 'http://localhost:5002/insertar/:codigo_vuelo';
-var UrlApiUpdate = 'http://localhost:5002/actualizar/:codigo_vuelo';
-var UrlApiDelete = 'http://localhost:5002/eliminar/:codigo_vuelo';
+var UrlApiUpdate = 'http://localhost:5002/vuelo/actualizar/:codigo_vuelo';
+var UrlApiDelete = 'http://localhost:5002/vuelo/eliminar/:codigo_vuelo';
 
 $(document).ready(function(){
-    CargarVuelo();
+    CargarVuelos();
 })
 
-function CargarVuelo(){
+function CargarVuelos(){
  $.ajax({
     url:UrlApiGetAll,
     type: 'GET',
@@ -27,10 +27,78 @@ function CargarVuelo(){
             '<td>'+ MiItems[i].tipo_avion +'</td>'+
             '<td>'+ MiItems[i].distancia_km +'</td>'+
             '<td>'+
+            '<button id="btneditar" class="btn btn-primary" onclick="CargarVuelo('+ MiItems[i].codigo_vuelo +')">Editar</button>'+
             '</td>'+
             '</tr>';
-            $('#DataVuelo').html(Valores);
+            $('#DataVuelos').html(Valores);
         }
     }
  });
+}
+
+function AgregarVuelo(){
+    var datosvuelo = {
+        codigo_vuelo :$('#CODIGOVUE').val(),
+        ciudad_origen :$('#CIUDADORI').val(),
+        ciudad_destino :$('#CIUDADDES').val(),
+        fecha_vuelo :$('#FECHAVUE').val(),
+        cantidad_pasajeros :$('#CANTIDADPAS').val(),
+        tipo_avion :$('#TIPOAVI').val(),
+        distancia_km :$('#DISTANCIA').val()
+    };
+
+    var datosvuelojson = JSON.stringify(datosvuelo);
+    //alert(datosvuelojson)
+
+    $.ajax({
+        url : UrlApiInsert,
+        type: 'POST',
+        data : datosvuelojson,
+        datatype: 'JSON',
+        contentType : 'application/json',
+        success : function(respuesta){
+            //console.log(respuesta)
+            alert('Vuelo ingresado de forma correcta');
+            $('#Miformulario').submit();
+        },
+        error : function(textError, errorThrown){
+            alert('Error '+textError+ errorThrown);
+        }
+    });
+    alert('Aviso');
+}
+
+function CargarVuelo(p_codigo_vuelo){
+    var datosvuelo = {
+        codigo_vuelo : p_codigo_vuelo
+    };
+
+    var datosvuelojson = JSON.stringify(datosvuelo);
+    //alert(datosvuelojson)
+
+    $.ajax({
+        url : UrlApiGetUno,
+        type: 'POST',
+        data : datosvuelojson,
+        datatype: 'JSON',
+        contentType : 'application/json', 
+        success : function(response){
+            var MiItems = response;
+            for(i=0; i < MiItems.length; i++){
+                $('#CODIGOVUE').val(MiItems[i].codigo_vuelo);
+                $('#CIUDADORI').val(MiItems[i].ciudad_origen);
+                $('#CIUDADDES').val(MiItems[i].ciudad_destino);
+                $('#FECHAVUE').val(MiItems[i].fecha_vuelo);
+                $('#CANTIDADPAS').val(MiItems[i].cantidad_pasajeros);
+                $('#TIPOAVI').val(MiItems[i].tipo_avion);
+                $('#DISTANCIA').val(MiItems[i].distancia_km);
+                var btnactualizar = '<input type="submit" class="btn btn-primary" id="btnagregar" onclick="ActualizarVuelo('+ MiItems[i].codigo_vuelo +')" value="Actualizar Vuelo">';
+                $('#btnagregarvuelo').html(btnactualizar)
+            }
+        },
+    });
+}
+
+function ActualizarVuelo(p_codigo_vuelo){
+
 }
